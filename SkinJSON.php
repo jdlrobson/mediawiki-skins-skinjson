621d858fd47d5c561c1aa81b236b002c187fa0db
jdlrobson <jdlrobson@gmail.com>
2020-12-20 12:36:50 -0800

    Support CORs

--- SkinJSON.php
+++ SkinJSON.php
@@ -12,7 +12,12 @@ class SkinJSON extends SkinMustache {
 
 		$this->initPage( $out );
 		$out->addJsConfigVars( $this->getJsConfigVars() );
-		$this->getRequest()->response()->header( 'Content-Type: application/json' );
+		$response = $this->getRequest()->response();
+		$response->header( 'Access-Control-Allow-Headers: ' . $requestedHeaders );
+		$response->header( 'Content-Type: application/json' );
+		$response->header( 'Cache-Control: no-cache' );
+		$response->header( 'Access-Control-Allow-Methods: GET' );
+		$response->header( 'Access-Control-Allow-Origin: *' );
 		// result may be an error
 		echo $this->generateHTML();
 	}
