419b7081ade7adfbcdc2e618a743db115adbe4fd
jdlrobson <jdlrobson@gmail.com>
2021-05-18 16:08:27 -0700

    Allow useskin=vector&useformat=json

--- SkinJSON.php
+++ SkinJSON.php
@@ -1,10 +1,31 @@
 <?php
+
+use MediaWiki\MediaWikiServices;
+
 class SkinJSON extends SkinMustache {
+	private $loadedTemplateData;
 	public function generateHTML() {
 		$this->setupTemplateContext();
-		$out = $this->getOutput();
 		$data = $this->getTemplateData();
 		return json_encode( $data );
+	}
+
+	/**
+	 * Returns template data for the skin from cached data or from core.
+	 */
+	function getTemplateData() {
+		if ($this->loadedTemplateData) {
+			return $this->loadedTemplateData;
+		} else {
+			return parent::getTemplateData();
+		}
+	}
+
+	/**
+	 * Loads template data from another skin into SkinJSON so SkinJSON functions as a proxy.
+	 */
+	function loadTemplateData( $data ) {
+		$this->loadedTemplateData = $data;
 	}
 
 	function getUser() {
@@ -15,6 +36,31 @@ class SkinJSON extends SkinMustache {
 			return $testUser;
 		}
 		return parent::getUser();
+	}
+
+	public static function onRequestContextCreateSkin( $context, &$skin ) {
+		$reqSkinKey = $context->getRequest()->getVal( 'useskin' );
+		$format = $context->getRequest()->getVal( 'useformat' );
+		if ( $skin === null && $reqSkinKey && $format === 'json' ) {
+			$services = MediaWikiServices::getInstance();
+			$factory = MediaWikiServices::getInstance()->getSkinFactory();
+			$reqSkin = $factory->makeSkin( $reqSkinKey );
+			$skin = $factory->makeSkin( Skin::normalizeKey( 'skinjson' ) );
+			// Stop the hook from running again.
+			$context->setSkin( $skin );
+
+			if ( method_exists( $reqSkin, 'getTemplateData' ) ) {
+				//$data = $reqSkin->getTemplateData();
+				// Wasteful, but makes sure skin gets initialized. setupTemplateContext is protected.
+				$html = $reqSkin->generateHTML();
+				$skin->loadTemplateData( $reqSkin->getTemplateData() );
+			} else {
+				$skin->loadTemplateData( [
+					'error' => 'Skin ' . $reqSkinKey . ' does not use SkinMustache and is not supported by SkinJSON',
+				] );
+			}
+		}
+		return false;
 	}
 
 	function outputPage() {
